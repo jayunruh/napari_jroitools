2bc25e0a77446ab3ddff68456723bb056ec07ed4
Unruh <jru@stowers.org>
2021-03-09 09:29:50 -0600

    update

--- update_github.bat
+++
@@ -1,5 +0,0 @@
-copy * "c:\Users\jru\Documents\GitHub\napari_jroitools\"
-cd "c:\Users\jru\Documents\GitHub\napari_jroitools"
-git add *
-git commit -m "update"
-git push --all --repo=https://jayunruh@github.com/jayunruh/napari_jroitools.git
